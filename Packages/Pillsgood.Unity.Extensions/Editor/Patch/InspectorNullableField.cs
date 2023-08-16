5155e9d05477d48f42d509bdab868a5121a30519
Mark <arashm35@gamesoultech.com>
2023-08-16 10:35:26 +0800

    Enhance nullability check in InspectorNullableField
    Additional functionality has been added to better support nullability detection in field types of the InspectorNullableField class. The prior `IsNullableMember` method was renamed to `IsNullable`, and substantially expanded to include inspecting custom attributes attached to the field.
    
    Newly added code properly inspects nullable value types, deals with the System.Runtime.CompilerServices.NullableAttribute and handles the NullableContextAttribute for containing types. This thorough checking enhances interoperability and stability for nullable types in UnityEditor, while also providing more context for fields that can be null.

--- Packages/Pillsgood.Unity.Extensions/Editor/Patch/InspectorNullableField.cs
+++ Packages/Pillsgood.Unity.Extensions/Editor/Patch/InspectorNullableField.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection;
 using HarmonyLib;
@@ -38,15 +40,61 @@ namespace Pillsgood.Unity.Extensions.Editor.Patch
             if (type is null) return;
             if (type.IsValueType) return;
 
-            if (fieldInfo != null && IsNullableMember(fieldInfo))
+            if (fieldInfo != null && IsNullable(fieldInfo))
             {
                 __result += "?";
             }
         }
 
-        private static bool IsNullableMember(MemberInfo fieldInfo)
+        private static bool IsNullable(FieldInfo field)
         {
-            return fieldInfo.CustomAttributes.Any(x => x.AttributeType.Name is "NullableAttribute");
+            return IsNullableHelper(field.FieldType, field.DeclaringType, field.CustomAttributes);
+        }
+
+        private static bool IsNullableHelper(
+            Type memberType,
+            MemberInfo? declaringType,
+            IEnumerable<CustomAttributeData> customAttributes)
+        {
+            if (memberType.IsValueType)
+            {
+                return Nullable.GetUnderlyingType(memberType) != null;
+            }
+
+            var nullable = customAttributes
+                .FirstOrDefault(x => x.AttributeType.FullName == "System.Runtime.CompilerServices.NullableAttribute");
+            if (nullable != null && nullable.ConstructorArguments.Count == 1)
+            {
+                var attributeArgument = nullable.ConstructorArguments[0];
+                if (attributeArgument.ArgumentType == typeof(byte[]))
+                {
+                    var args = (ReadOnlyCollection<CustomAttributeTypedArgument>)attributeArgument.Value!;
+                    if (args.Count > 0 && args[0].ArgumentType == typeof(byte))
+                    {
+                        return (byte)args[0].Value! == 2;
+                    }
+                }
+                else if (attributeArgument.ArgumentType == typeof(byte))
+                {
+                    return (byte)attributeArgument.Value! == 2;
+                }
+            }
+
+            for (var type = declaringType; type != null; type = type.DeclaringType)
+            {
+                var context = type.CustomAttributes
+                    .FirstOrDefault(x =>
+                        x.AttributeType.FullName == "System.Runtime.CompilerServices.NullableContextAttribute");
+                if (context != null &&
+                    context.ConstructorArguments.Count == 1 &&
+                    context.ConstructorArguments[0].ArgumentType == typeof(byte))
+                {
+                    return (byte)context.ConstructorArguments[0].Value! == 2;
+                }
+            }
+
+            // Couldn't find a suitable attribute
+            return false;
         }
     }
 }
