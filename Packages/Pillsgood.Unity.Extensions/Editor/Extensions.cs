a7d63327c991b63ee607dbe97b21302842e961fe
Mark <arashm35@gamesoultech.com>
2023-11-07 17:09:30 +0800

    Add cancellation token and enhance InspectorTitlebar
    Implemented CancellationToken in Extensions.cs to handle script reloading interruptions. Enhanced InspectorTitlebar.cs by adding new patches so as to improve the handling and display of inspector title bars. The modifications aim to yield better visual representation and boost performance by cancelling long running tasks during script reloads and managing the titlebar's behavior more efficiently.

--- Packages/Pillsgood.Unity.Extensions/Editor/Extensions.cs
+++ Packages/Pillsgood.Unity.Extensions/Editor/Extensions.cs
@@ -1,11 +1,17 @@
-﻿using HarmonyLib;
+﻿using System;
+using System.Threading;
+using HarmonyLib;
+using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEngine;
+using Task = System.Threading.Tasks.Task;
 
 namespace Pillsgood.Unity.Extensions.Editor
 {
     internal static class Extensions
     {
+        private static CancellationTokenSource? _cts;
+
         [DidReloadScripts]
         private static void OnDomainReload()
         {
@@ -13,6 +19,19 @@ namespace Pillsgood.Unity.Extensions.Editor
             {
                 return;
             }
+
+            _cts?.Cancel();
+            _cts = new CancellationTokenSource();
+
+            var cancellationToken = _cts.Token;
+            PatchAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        private static async Task PatchAsync(CancellationToken cancellationToken)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+
+            if (cancellationToken.IsCancellationRequested) return;
 
             var harmony = new Harmony("com.pillsgood.extensions");
             harmony.PatchAll();
--- Packages/Pillsgood.Unity.Extensions/Editor/Patch/InspectorTitlebar.cs
+++ Packages/Pillsgood.Unity.Extensions/Editor/Patch/InspectorTitlebar.cs
@@ -1,5 +1,8 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
 using HarmonyLib;
 using JetBrains.Annotations;
 using Pillsgood.Unity.Extensions.Editor.Preferences;
@@ -46,6 +49,13 @@ namespace Pillsgood.Unity.Extensions.Editor.Patch
             throw new InvalidOperationException();
         }
 
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(EditorStyles), "inspectorTitlebar", MethodType.Getter)]
+        private static GUIStyle GetInspectorTitlebar()
+        {
+            throw new InvalidOperationException();
+        }
+
         [HarmonyPrefix, UsedImplicitly]
         [HarmonyPatch(typeof(ObjectNames), nameof(ObjectNames.GetInspectorTitle), typeof(Object), typeof(bool))]
         private static bool Prefix_ObjectNames_GetInspectorTitle(
@@ -79,37 +89,91 @@ namespace Pillsgood.Unity.Extensions.Editor.Patch
             return true;
         }
 
-        [HarmonyPostfix, UsedImplicitly]
-        [HarmonyPatch(typeof(ObjectNames), nameof(ObjectNames.GetInspectorTitle), typeof(Object), typeof(bool))]
-        private static void Postfix_ObjectNames_GetInspectorTitle(
-            ref string __result,
-            Object obj,
-            bool __state)
+        [HarmonyPatch(typeof(EditorGUI), "DoInspectorTitlebar")]
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> Transpile_EditorGUI_DoInspectorTitlebar(
+            IEnumerable<CodeInstruction> instructions)
         {
-            if (obj is ScriptableObject) return;
+            yield return new CodeInstruction(OpCodes.Ldarg_S, 3);
+            yield return new CodeInstruction(OpCodes.Ldarg_S, 5);
 
-            if (!__state) __result = $"<b>{__result}</b>";
+            yield return CodeInstruction.Call(
+                (Expression<Func<Object[], GUIStyle, GUIStyle>>)((objs, style) => UpdateInspectorTitlebar(objs, style)));
+            yield return new CodeInstruction(OpCodes.Starg_S, 5);
+
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Stloc_0)
+                {
+                    yield return instruction;
+                    yield return new CodeInstruction(OpCodes.Ldarg_S, 3);
+                    yield return new CodeInstruction(OpCodes.Ldloc_0);
+                    yield return CodeInstruction.Call(
+                        (Expression<Func<Object[], GUIStyle, GUIStyle>>)((objs, style) => UpdateInspectorTitlebarText(objs, style)));
+                    yield return new CodeInstruction(OpCodes.Stloc_0);
+                    continue;
+                }
+
+                yield return instruction;
+            }
         }
 
-        [SuppressMessage("ReSharper", "InconsistentNaming")]
-        [HarmonyPostfix, UsedImplicitly]
-        [HarmonyPatch(typeof(EditorStyles), nameof(inspectorTitlebar), MethodType.Getter)]
-        private static void inspectorTitlebar(ref GUIStyle __result)
+        [HarmonyPatch(
+            typeof(EditorGUILayout),
+            nameof(EditorGUILayout.InspectorTitlebar),
+            typeof(bool), typeof(UnityEditor.Editor))]
+        [HarmonyPrefix]
+        private static bool Prefix_EditorGUILayout_InspectorTitlebar(
+            ref bool __result,
+            bool foldout,
+            UnityEditor.Editor editor)
         {
-            if (!Options.Enabled.Value) return;
-
-            __result.fixedHeight = Options.Height.Value;
+            var inspectorTitlebar = GetInspectorTitlebar();
+            inspectorTitlebar = UpdateInspectorTitlebar(editor.targets, inspectorTitlebar);
+            __result = EditorGUI.InspectorTitlebar(GUILayoutUtility.GetRect(GUIContent.none, inspectorTitlebar), foldout, editor);
+            return false;
         }
 
-        [SuppressMessage("ReSharper", "InconsistentNaming")]
-        [HarmonyPostfix, UsedImplicitly]
-        [HarmonyPatch(typeof(EditorStyles), nameof(inspectorTitlebarText), MethodType.Getter)]
-        private static void inspectorTitlebarText(ref GUIStyle __result)
+        private static GUIStyle UpdateInspectorTitlebar(Object[] objs, GUIStyle style)
         {
-            if (!Options.Enabled.Value) return;
+            if (!Options.Enabled.Value) return style;
 
-            __result.fixedHeight = Options.Height.Value - 2;
-            __result.fontStyle = FontStyle.Normal;
+            var obj = objs.FirstOrDefault();
+            if (obj is MonoBehaviour monoBehaviour)
+            {
+                var type = monoBehaviour.GetType();
+
+                if (TryGetInspectorTitle(type, out _)) return style;
+
+                return new GUIStyle(style)
+                {
+                    fixedHeight = Options.Height.Value
+                };
+            }
+
+            return style;
+        }
+
+        private static GUIStyle UpdateInspectorTitlebarText(Object[] objects, GUIStyle style)
+        {
+            if (!Options.Enabled.Value) return style;
+
+            var obj = objects.FirstOrDefault();
+            if (obj is MonoBehaviour monoBehaviour)
+            {
+                var type = monoBehaviour.GetType();
+
+                if (TryGetInspectorTitle(type, out _)) return style;
+
+
+                return new GUIStyle(style)
+                {
+                    fixedHeight = Options.Height.Value - 2,
+                    fontStyle = FontStyle.Normal
+                };
+            }
+
+            return style;
         }
     }
 }
